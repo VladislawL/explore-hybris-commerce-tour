6f59c6159b951394ba7d6228f4a1cb55a0345910
VladislawL <vladislaw.logvin@yandex.ru>
2020-09-18 12:38:13 +0300

    Added unique modifier to Producer's code attribute

--- bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEventListener.java
+++ bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEventListener.java
@@ -1,14 +1,21 @@
 package concerttours.events;
+import de.hybris.platform.catalog.CatalogVersionService;
 import de.hybris.platform.servicelayer.event.impl.AbstractEventListener;
+import de.hybris.platform.servicelayer.keygenerator.KeyGenerator;
 import de.hybris.platform.servicelayer.model.ModelService;
 import java.util.Date;
 import concerttours.model.NewsModel;
+import org.springframework.beans.factory.annotation.Required;
 
 public class BandAlbumSalesEventListener extends AbstractEventListener<BandAlbumSalesEvent>
 {
     private static final String BAND_SALES_HEADLINE = "%s album sales exceed 50000";
     private static final String BAND_SALES_CONTENT = "%s album sales reported as %d";
+    private static final String CATALOG_ID = "concertoursProductCatalog";
+    private static final String CATALOG_VERSION_NAME = "Online";
     private ModelService modelService;
+    private CatalogVersionService catalogVersionService;
+    private KeyGenerator keyGenerator;
     public ModelService getModelService()
     {
         return modelService;
@@ -25,10 +32,22 @@ public class BandAlbumSalesEventListener extends AbstractEventListener<BandAlbum
             final String headline = String.format(BAND_SALES_HEADLINE, event.getName());
             final String content = String.format(BAND_SALES_CONTENT, event.getName(), event.getSales());
             final NewsModel news = modelService.create(NewsModel.class);
+            news.setId(keyGenerator.generate().toString());
+            news.setCatalogVersion(catalogVersionService.getCatalogVersion(CATALOG_ID, CATALOG_VERSION_NAME));
             news.setDate(new Date());
             news.setHeadline(headline);
             news.setContent(content);
             modelService.save(news);
         }
+    }
+
+    @Required
+    public void setCatalogVersionService(CatalogVersionService catalogVersionService) {
+        this.catalogVersionService = catalogVersionService;
+    }
+
+    @Required
+    public void setKeyGenerator(KeyGenerator keyGenerator) {
+        this.keyGenerator = keyGenerator;
     }
 }
--- bin/custom/concerttours/src/concerttours/events/NewBandEventListener.java
+++ bin/custom/concerttours/src/concerttours/events/NewBandEventListener.java
@@ -1,16 +1,34 @@
 package concerttours.events;
+import de.hybris.platform.catalog.CatalogVersionService;
 import de.hybris.platform.servicelayer.event.events.AfterItemCreationEvent;
 import de.hybris.platform.servicelayer.event.impl.AbstractEventListener;
+import de.hybris.platform.servicelayer.keygenerator.KeyGenerator;
 import de.hybris.platform.servicelayer.model.ModelService;
 import java.util.Date;
 import concerttours.model.BandModel;
 import concerttours.model.NewsModel;
+import org.springframework.beans.factory.annotation.Required;
 
 public class NewBandEventListener extends AbstractEventListener<AfterItemCreationEvent>
 {
+    private static final String CATALOG_ID = "concertoursProductCatalog";
+    private static final String CATALOG_VERSION_NAME = "Online";
     private static final String NEW_BAND_HEADLINE = "New band, %s";
     private static final String NEW_BAND_CONTENT = "There is a new band in town called, %s. Tour news to be announced soon.";
     private ModelService modelService;
+    private CatalogVersionService catalogVersionService;
+    private KeyGenerator keyGenerator;
+
+    @Required
+    public void setCatalogVersionService(CatalogVersionService catalogVersionService) {
+        this.catalogVersionService = catalogVersionService;
+    }
+
+    @Required
+    public void setKeyGenerator(KeyGenerator keyGenerator) {
+        this.keyGenerator = keyGenerator;
+    }
+
     public ModelService getModelService()
     {
         return modelService;
@@ -31,6 +49,8 @@ public class NewBandEventListener extends AbstractEventListener<AfterItemCreatio
                 final String headline = String.format(NEW_BAND_HEADLINE, band.getName());
                 final String content = String.format(NEW_BAND_CONTENT, band.getName());
                 final NewsModel news = modelService.create(NewsModel.class);
+                news.setId(keyGenerator.generate().toString());
+                news.setCatalogVersion(catalogVersionService.getCatalogVersion(CATALOG_ID, CATALOG_VERSION_NAME));
                 news.setDate(new Date());
                 news.setHeadline(headline);
                 news.setContent(content);
